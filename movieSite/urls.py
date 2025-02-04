cd78ddcc656b784b662b851a0bf83e89bac42491
eahmed7 <ehanahmed@lawn-10-91-33-141.lawn.gatech.edu>
2025-02-04 18:19:51 -0500

    ehans changes

--- movieSite/urls.py
+++ movieSite/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='movieStore.index'),
+    path('about/', views.about, name='movieStore.about'),
 ]
--- movieSite/views.py
+++ movieSite/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'movieSite/index.html')
+def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'movieSite/about.html',{'template_data': template_data})
