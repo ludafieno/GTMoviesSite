7f61b4c785c1e352807aa85d2fbfa9e100bff1cb
ludafieno <ludafieno@outlook.com>
2025-02-11 15:48:50 -0500

    cart system;adding order, clearing orders. up to ch 11

--- GTMoviesSite/urls.py
+++ GTMoviesSite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('movieSite.urls')),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
