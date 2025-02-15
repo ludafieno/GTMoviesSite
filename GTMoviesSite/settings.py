e2a1371b62bfe84588f4f00253a9f81b2d2c8266
smurley6 <steven.murley@gmail.com>
2025-02-14 20:21:45 -0500

    Update settings.py

--- GTMoviesSite/settings.py
+++ GTMoviesSite/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "GTMoviesSite", "static"),
